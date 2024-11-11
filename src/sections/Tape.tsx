eabf3cc0a36fc72e95cb20120019a0237cd08352
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-11-11 22:58:50 +0530

    feat : Tape Section designed

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -18,10 +18,10 @@ export const TapeSection = () => {
     "Results-Oriented"
   ];
   
-  return <div className="py-16 ">
+  return <div className="py-16 lg:py-24 overflow-x-clip ">
     {/* gradientstrip */}
-    <div className="gradientstrip  bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip">
-    <div className="flex">
+    <div className="gradientstrip  bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
+    <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
 
     <div className="wordcont flex flex-none gap-4 py-3">
     {
@@ -30,7 +30,7 @@ export const TapeSection = () => {
         
         <div key={skill} className="inline-flex gap-4 items-center">
           <span className="text-gray-900 uppercase font-extrabold text-sm">{skill}</span>
-          <StarIcon className ="size-6 text-gray-900"/>
+          <StarIcon className ="size-6 text-gray-900 -rotate-12"/>
         </div>
         
       ))
